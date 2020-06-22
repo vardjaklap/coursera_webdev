2def7a813fbae8c69e5d25ebf28c3ab30bb53075
vardjaklap <alexperesta@gmail.com>
2020-06-22 16:41:51 -0400

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
                      "randomCategoryShortName",
                       "'" + chosenCategoryShortName + "'");
 
-
+      console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
